41c315799043678f4fcfebeecc1f188e1a14a77a
IslamRamadan <iniestazsc@users.noreply.github.com>
2020-04-10 05:21:03 +0200

    my first commit

---
+++ resources/assets/js/components/BarNav.vue
@@ -0,0 +1,15 @@
+<template>
+    <nav class="navbar navbar-expand-sm navbar-dark bg-info mb2">
+        <a href="#" class="navbar-brand">Larticles</a>
+    </nav>
+</template>
+
+
+<script>
+    export default {
+        name: 'barnav',
+        mounted() {
+            console.log('Component Example Two.')
+        }
+    }
+</script>
---
+++ resources/assets/js/components/NavBar.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container">
+        <h1>test</h1>
+    </div>
+
+</template>
+
+
+<script>
+    export default {
+        name: 'examples',
+        mounted() {
+            console.log('Component Example Two.')
+        }
+    }
+</script>
